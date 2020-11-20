83f806a752d1af56836806e5de2eeaae1039075f
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2020-11-20 16:56:56 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+LeetCode - Category - Algorithms
+========
+
+| # | Title | Solution | Difficulty |
+|---| ----- | -------- | ---------- |
+| 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | [Java](./java/TwoSum.java)  | Easy |
---
+++ java/TwoSum.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (nums[i] + nums[j] == target)
+          return new int []{i, j};
+      }
+    }
+    return new int []{-1, -1};
+  }
+}
