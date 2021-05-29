b98de22fe919ca8758821c8c37334024733c2d73
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-05-29 19:27:33 +0600

    Added solution to Isomorphic Strings

---
+++ c++/IsomorphicStrings.cpp
@@ -0,0 +1,30 @@
+class IsomorphicStrings {
+public:
+  bool isIsomorphic(string s, string t) {
+    int len = s.size();
+    bool isIso = true;
+    unordered_map<char, char> mapping;
+    unordered_map<char, char> mappedTo;
+    
+    for (int i = 0; i < len; i++) {
+      if (mapping.count(s[i])) {
+        char ch = mapping[s[i]];
+        
+        if (t[i] != ch) {
+          isIso = false;
+          break;
+        }
+      } else {
+        if (mappedTo.count(t[i])) {
+          isIso = false;
+          break;
+        }
+        
+        mapping[s[i]] = t[i];
+        mappedTo[t[i]] = s[i];
+      }
+    }
+    
+    return isIso;
+  }
+};
