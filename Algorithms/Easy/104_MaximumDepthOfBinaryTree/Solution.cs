7ffd59c54fdf3a258e124d5915c0f044da5b946f
Nakhla Rafi <nakhla.rafi@g.bracu.ac.bd>
2021-07-26 14:11:32 +0600

    104. Maximum Depth of Binary Tree Solution added (#9)
    * 104. Maximum Depth of Binary Tree Solution added
    
    * resolved
    
    * resubbmitted

---
+++ Algorithms/Easy/104_MaximumDepthOfBinaryTree/Solution.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftTree = MaxDepth(root.left) + 1;
+        int rightTree = MaxDepth(root.right) + 1;
+        return leftTree >= rightTree ? leftTree : rightTree; 
+    }
+}
