24936fccf1e74022d38dc3d1ce102cb2fd1f8c7e
nakhlarafi <nakhla.rafi@g.bracu.ac.bd>
2021-07-29 21:16:46 +0600

    changes added

--- Algorithms/Hard/4_MedianofTwoSortedArrays/Solution.java
+++ Algorithms/Hard/4_MedianofTwoSortedArrays/Solution.java
@@ -1,39 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         double median = 0.0;
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int arr[] = new int[n1 + n2];
-        int i = 0, j = 0; 
-        int k = 0; 
-        while (i < n1 && j < n2) { 
+        int m = nums1.length;
+        int n = nums2.length;
+        int combinedArr[] = new int[m+n];
+        int i = 0, j = 0, k = 0; 
+        
+        while (i < m && j < n) { 
             if (nums1[i] <= nums2[j]) { 
-                arr[k] = nums1[i]; 
+                combinedArr[k] = nums1[i]; 
                 i++; 
             } 
             else { 
-                arr[k] = nums2[j]; 
+                combinedArr[k] = nums2[j]; 
                 j++; 
             } 
             k++; 
         }
-        while (i < n1) { 
-            arr[k] = nums1[i]; 
+        
+        while (i < m) { 
+            combinedArr[k] = nums1[i]; 
             i++; 
             k++; 
         } 
         
-        while (j < n2) { 
-            arr[k] = nums2[j]; 
+        while (j < n) { 
+            combinedArr[k] = nums2[j]; 
             j++; 
             k++; 
         }
-              
-        if(arr.length % 2 == 0){
-            median = (arr[(arr.length / 2) - 1] + arr[arr.length / 2]) / 2.0;
+        
+        if(combinedArr.length%2 == 0){
+            median = (combinedArr[(combinedArr.length/2)-1] + combinedArr[combinedArr.length/2]) / 2.0;
         }
         else{
-            median = (arr[arr.length / 2]) / 1.0;
+            median = (combinedArr[combinedArr.length/2]) / 1.0;
         }
         return median;   
     }
