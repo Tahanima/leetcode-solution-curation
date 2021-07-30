ee991a14aa5997c2c5631b3cb595f09da682cb8f
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-07-30 12:41:47 +0600

    Fixed formatting issues (#27)

--- Algorithms/Hard/4_MedianofTwoSortedArrays/Solution.java
+++ Algorithms/Hard/4_MedianofTwoSortedArrays/Solution.java
@@ -3,18 +3,16 @@ class Solution {
         double median = 0.0;
         int m = nums1.length;
         int n = nums2.length;
-        int combinedArr[] = new int[m+n];
+        int combinedArr[] = new int[m + n];
         int i = 0, j = 0, k = 0; 
         
         while (i < m && j < n) { 
             if (nums1[i] <= nums2[j]) { 
-                combinedArr[k] = nums1[i]; 
-                i++; 
-            } 
-            else { 
-                combinedArr[k] = nums2[j]; 
-                j++; 
-            } 
+                combinedArr[k] = nums1[i++]; 
+            } else { 
+                combinedArr[k] = nums2[j++]; 
+            }
+            
             k++; 
         }
         
@@ -30,12 +28,12 @@ class Solution {
             k++; 
         }
         
-        if(combinedArr.length % 2 == 0){
-            median = (combinedArr[(combinedArr.length/2) - 1] + combinedArr[combinedArr.length/2]) / 2.0;
-        }
-        else{
+        if (combinedArr.length % 2 == 0) {
+            median = (combinedArr[(combinedArr.length / 2) - 1] + combinedArr[combinedArr.length / 2]) / 2.0;
+        } else {
             median = (combinedArr[combinedArr.length / 2]) / 1.0;
         }
+        
         return median;   
     }
 }
