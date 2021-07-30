2ead9bbdf7220206f511e623c35b273359df3541
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-07-30 20:32:44 +0600

    Add LeetCode 183

---
+++ Database/Easy/183_CustomersWhoNeverOrder.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+SELECT Customers.Name AS Customers
+FROM Customers
+LEFT JOIN Orders ON Customers.Id = Orders.CustomerId
+WHERE Orders.CustomerId IS NULL;
