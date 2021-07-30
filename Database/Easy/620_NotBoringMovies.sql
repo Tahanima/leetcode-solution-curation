34db198ddfabe0fd1a320e7bef07660366afe6d0
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-07-30 20:53:27 +0600

    Add LeetCode 620

---
+++ Database/Easy/620_NotBoringMovies.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+SELECT id, movie, description, rating
+FROM cinema
+WHERE (id LIKE '%1' OR id LIKE '%3' OR id LIKE '%5' OR id LIKE '%7' OR id LIKE '%9') AND description NOT IN ('boring')
+ORDER BY rating DESC;
