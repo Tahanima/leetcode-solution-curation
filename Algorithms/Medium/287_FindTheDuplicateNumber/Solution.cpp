e258009cd69e8a8405e615db215a506b5d08f458
Amisha Sahu <amishasahu328@gmail.com>
2021-07-31 13:23:16 +0530

    Added Solution to problem 287
    Signed-off-by: Amisha Sahu <amishasahu328@gmail.com>

---
+++ Algorithms/Medium/287_FindTheDuplicateNumber/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n =nums.size();
+        for(int i = 0;i<n-1;i++)
+        {
+            if(nums[i] == nums[i+1])
+                return nums[i];
+        }
+        return -1;
+    }
+};
