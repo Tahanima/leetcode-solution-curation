2ba4b52f1561a452cf3571aba9389a24136b5c42
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2021-07-31 18:42:07 +0530

    Updated the code spacing and complexity.

--- Algorithms/Medium/287_FindTheDuplicateNumber/Solution.cpp
+++ Algorithms/Medium/287_FindTheDuplicateNumber/Solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n =nums.size();
-        for(int i = 0;i<n-1;i++)
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+       for (int i = 0; i < n-1; i++)
         {
             if(nums[i] == nums[i+1])
                 return nums[i];
@@ -12,5 +12,5 @@ public:
     }
 };
 
-// Time Complexity: O(n)
+// Time Complexity: O(nlogn)
 // Space Complexity: O(n)
