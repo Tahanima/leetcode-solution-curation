3d6d053f489dd4e8064b1f7c1013735d07ce4d15
r8025n <raihanshovon95@gmail.com>
2021-08-01 00:12:11 +0600

    Address requested changes

--- Algorithms/Easy/112_PathSum/Solution.c
+++ Algorithms/Easy/112_PathSum/Solution.c
@@ -7,16 +7,15 @@
  * };
  */
 
-bool hasPathSum(struct TreeNode* root, int targetSum){
+bool hasPathSum(struct TreeNode* root, int targetSum) {
     if(root == NULL)
         return false;
-    if (root->left == NULL && root->right == NULL){
+
+    if (root->left == NULL && root->right == NULL) {
         if (root->val == targetSum)
             return true;
+
         return false;
     }
-    
     return (hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val));
 }
-
-
