39be15eba6636679d90bed66326f4d6bafdefe27
Hasin Raihan Shovon <43141944+r8025n@users.noreply.github.com>
2021-08-01 00:21:54 +0600

    LeetCode-112 (Path Sum) solution added (#40)
    * Add solution to LeetCode 112
    
    * Address requested changes

---
+++ Algorithms/Easy/112_PathSum/Solution.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool hasPathSum(struct TreeNode* root, int targetSum) {
+    if(root == NULL)
+        return false;
+
+    if (root->left == NULL && root->right == NULL) {
+        if (root->val == targetSum)
+            return true;
+
+        return false;
+    }
+    return (hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val));
+}
