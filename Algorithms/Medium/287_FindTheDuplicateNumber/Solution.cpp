7f957aa3fbc3c04c15488f52f84730fc552cf7dd
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2021-07-31 18:42:32 +0530

    Update Solution.cpp

--- Algorithms/Medium/287_FindTheDuplicateNumber/Solution.cpp
+++ Algorithms/Medium/287_FindTheDuplicateNumber/Solution.cpp
@@ -3,7 +3,7 @@ public:
     int findDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-       for (int i = 0; i < n-1; i++)
+        for (int i = 0; i < n-1; i++)
         {
             if(nums[i] == nums[i+1])
                 return nums[i];
