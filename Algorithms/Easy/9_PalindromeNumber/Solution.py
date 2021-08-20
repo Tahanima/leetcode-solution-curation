1c3ac7bdd823d30e1aec4e38a1b9e884e6643a6a
FahimFBA <fahimbinamin@gmail.com>
2021-08-21 02:44:26 +0600

    Added a blank line after return False

--- Algorithms/Easy/9_PalindromeNumber/Solution.py
+++ Algorithms/Easy/9_PalindromeNumber/Solution.py
@@ -2,6 +2,7 @@ class Solution(object):
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
+            
         b =int(str(x) [::-1])
         if b == x:
             return True
