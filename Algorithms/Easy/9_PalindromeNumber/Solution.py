cc00d18104e31ccd2b7b5f1e80fd7df236abe1c2
FahimFBA <fahimbinamin@gmail.com>
2021-08-21 02:43:04 +0600

    changed the 2nd line to the same as the LeetCode editor

--- Algorithms/Easy/9_PalindromeNumber/Solution.py
+++ Algorithms/Easy/9_PalindromeNumber/Solution.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def isPalindrome(self, x): # type x : int & return type: bool
+    def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
         b =int(str(x) [::-1])
