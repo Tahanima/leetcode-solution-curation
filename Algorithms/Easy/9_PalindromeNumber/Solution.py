f3dd142bcd5d752c09af60768afada429f65fc7c
FahimFBA <fahimbinamin@gmail.com>
2021-08-21 02:44:49 +0600

    fixed spacing

--- Algorithms/Easy/9_PalindromeNumber/Solution.py
+++ Algorithms/Easy/9_PalindromeNumber/Solution.py
@@ -2,8 +2,8 @@ class Solution(object):
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-            
-        b =int(str(x) [::-1])
+
+        b = int(str(x) [::-1])
         if b == x:
             return True
         return False
