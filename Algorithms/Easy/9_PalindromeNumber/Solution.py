59cd73ee49e3498845a60d7d02ac1e56c5dc3bca
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-08-21 18:08:11 +0600

    added solution of Palindrome Number in Python (#53)
    * added solution of Palindrome Number in Python
    
    * changed the 2nd line to the same as the LeetCode editor
    
    * Added a blank line after return False
    
    * fixed spacing
    
    * Added a blank line after return True

---
+++ Algorithms/Easy/9_PalindromeNumber/Solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        b = int(str(x) [::-1])
+        if b == x:
+            return True
+            
+        return False
