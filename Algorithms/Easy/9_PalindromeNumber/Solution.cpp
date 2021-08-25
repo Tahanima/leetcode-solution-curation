123f34da5100dfaf5dad147256c81ddc6cede3a3
IsratIJK <iptu.jahan.amin@gmail.com>
2021-08-25 15:26:20 +0600

    Added solution of Palindrome Number in c++

---
+++ Algorithms/Easy/9_PalindromeNumber/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       if(x < 0 || (x % 10 == 0 && x != 0)) {
+           return false;
+       }
+       int reverseNumber = 0;
+       while(x > reverseNumber) {
+           reverseNumber = (reverseNumber * 10) + (x % 10);
+           x = x / 10;
+       }
+        return x == reverseNumber || x == reverseNumber / 10;
+    }
+};
