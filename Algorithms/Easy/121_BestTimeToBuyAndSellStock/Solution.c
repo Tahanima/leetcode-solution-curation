554da13c7f29f1276712d24f7c3ec97ca29186bf
r8025n <raihanshovon95@gmail.com>
2021-09-07 20:18:10 +0600

    Add LeetCode 121 solution in C

---
+++ Algorithms/Easy/121_BestTimeToBuyAndSellStock/Solution.c
@@ -0,0 +1,13 @@
+int maxProfit(int* prices, int pricesSize){
+    int answer = 0;
+    int buyingPrice = prices[0];
+    
+    for(int i = 1; i < pricesSize; i++){
+        if(prices[i] < buyingPrice)
+            buyingPrice = prices[i];
+        else if (prices[i] - buyingPrice > answer)
+            answer = prices[i] - buyingPrice;
+    }
+    
+    return answer;
+}
