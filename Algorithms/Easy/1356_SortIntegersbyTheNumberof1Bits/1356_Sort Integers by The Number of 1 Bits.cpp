9f21c371b636816d1d5081c8c62a61a19971afd2
Manas Tiwari <46261625+M-Manas-s@users.noreply.github.com>
2021-10-01 09:20:43 +0530

    Edit

--- Algorithms/Easy/1356_SortIntegersbyTheNumberof1Bits/1356_Sort Integers by The Number of 1 Bits.cpp
+++ Algorithms/Easy/1356_SortIntegersbyTheNumberof1Bits/1356_Sort Integers by The Number of 1 Bits.cpp
@@ -4,28 +4,28 @@ using namespace std;
 class Solution {
 public:
     
-    static bool cmp(int a,int b)
+    static bool cmp ( int a, int b )
     {
         int bitsa = 0;
         int bitsb = 0;
-        int ta=a,tb=b;
-        while(a!=0)
+        int ta = a, tb = b;
+        while( a != 0 )
         {
-            if ( a%2==1 )
+            if ( a%2 == 1 )
                 bitsa++;
-            a/=2;
+            a /= 2;
         }
-        while(b!=0)
+        while( b != 0 )
         {
-            if ( b%2==1 )
+            if ( b%2 == 1 )
                 bitsb++;
-            b/=2;
+            b /= 2;
         }
-        return ( bitsa!=bitsb ? bitsa<bitsb : ta<tb);
+        return ( bitsa != bitsb ? bitsa < bitsb : ta < tb);
     }
     
-    vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(),arr.end(),cmp);
+    vector<int> sortByBits ( vector<int>& arr ) {
+        sort( arr.begin(), arr.end(), cmp );
         return arr;
     }
 };
