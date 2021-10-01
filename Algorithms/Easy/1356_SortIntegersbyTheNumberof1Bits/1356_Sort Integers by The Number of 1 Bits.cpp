7ac07265bbdeb5462eb2976923bf3864075c8f5b
M-Manas-s <manastiwari111222@gmail.com>
2021-10-01 09:14:58 +0530

    1356. Sort Integers by The Number of 1 Bits

---
+++ Algorithms/Easy/1356_SortIntegersbyTheNumberof1Bits/1356_Sort Integers by The Number of 1 Bits.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    static bool cmp(int a,int b)
+    {
+        int bitsa = 0;
+        int bitsb = 0;
+        int ta=a,tb=b;
+        while(a!=0)
+        {
+            if ( a%2==1 )
+                bitsa++;
+            a/=2;
+        }
+        while(b!=0)
+        {
+            if ( b%2==1 )
+                bitsb++;
+            b/=2;
+        }
+        return ( bitsa!=bitsb ? bitsa<bitsb : ta<tb);
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(),arr.end(),cmp);
+        return arr;
+    }
+};
