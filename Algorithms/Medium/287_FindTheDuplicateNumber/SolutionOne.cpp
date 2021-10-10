62df76303561e098eac98a58714fda9b71f54046
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-10-10 23:17:09 +0600

    Add LeetCode 87 cpp

---
+++ Algorithms/Medium/287_FindTheDuplicateNumber/SolutionOne.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        const int MX = 1e6 + 5;
+        bitset<MX> mask;
+        for (int i = 0; i < nums.size(); i++){
+            if (mask[nums[i]] == 1) return nums[i];
+            mask[nums[i]] = 1;
+        }
+        return 0;
+    }
+};
