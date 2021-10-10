723d246f180f1e7378254eb7038d5b0b0a01e572
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-10-10 22:55:48 +0600

    Update Solution.c

--- Algorithms/Easy/121_BestTimeToBuyAndSellStock/Solution.c
+++ Algorithms/Easy/121_BestTimeToBuyAndSellStock/Solution.c
@@ -2,8 +2,8 @@ int maxProfit(int* prices, int pricesSize){
     int answer = 0;
     int buyingPrice = prices[0];
     
-    for(int i = 1; i < pricesSize; i++){
-        if(prices[i] < buyingPrice)
+    for (int i = 1; i < pricesSize; i++){
+        if (prices[i] < buyingPrice)
             buyingPrice = prices[i];
         else if (prices[i] - buyingPrice > answer)
             answer = prices[i] - buyingPrice;
