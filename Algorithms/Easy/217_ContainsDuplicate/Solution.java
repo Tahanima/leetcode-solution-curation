6ca51f369e54222a31fe945cb020f161a69e4b89
MD. Maqsud ul Anwar <maqsud13101033@gmail.com>
2021-10-16 00:56:18 +0600

    Create Solution.java (#93)

---
+++ Algorithms/Easy/217_ContainsDuplicate/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) 
+                return true;
+        }
+        
+        return false;
+    }
+}
