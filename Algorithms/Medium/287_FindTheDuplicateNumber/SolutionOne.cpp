d202f54c0e0ffc3462908461927f6940e15312c9
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-28 22:45:52 +0600

    Update SolutionOne.cpp

--- Algorithms/Medium/287_FindTheDuplicateNumber/SolutionOne.cpp
+++ Algorithms/Medium/287_FindTheDuplicateNumber/SolutionOne.cpp
@@ -3,7 +3,8 @@ public:
     int findDuplicate(vector<int>& nums) {
         const int MX = 1e6 + 5;
         bitset<MX> mask;
-        for (int i = 0; i < nums.size(); i++){
+        
+        for (int i = 0; i < nums.size(); i++) {
             if (mask[nums[i]] == 1) return nums[i];
             mask[nums[i]] = 1;
         }
