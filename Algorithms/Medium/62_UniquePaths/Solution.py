1831a1a75351f40fb1aa5c2de64ddef2c7d20869
Joy Krishan Das <joykrishan10@gmail.com>
2022-10-15 17:02:54 +0600

    Changes added to Python solution for 62. UniquePaths

--- Algorithms/Medium/62_UniquePaths/Solution.py
+++ Algorithms/Medium/62_UniquePaths/Solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        bottomRow = [1]*n
+        bottom_row = [1] * n
         
         for column in range(m - 1):
-            newRow = [1]*n
-            for i in range(n-2, -1, -1):
-                newRow[i] = newRow[i + 1] + bottomRow[i]
-            bottomRow = newRow
+            new_row = [1] * n
+
+            for i in range(n - 2, -1, -1):
+                new_row[i] = new_row[i + 1] + bottom_row[i]
+            bottom_row = new_row
         
-        return bottomRow[0]
+        return bottom_row[0]
