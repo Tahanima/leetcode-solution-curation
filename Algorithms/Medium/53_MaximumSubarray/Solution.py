335bcf88d9ce436edb54b2c30e5a609f7611575a
Joy Krishan Das <joykrishan10@gmail.com>
2022-10-15 13:32:00 +0600

    Changes added to Python solution for 53. MaximumSubarray

---
+++ Algorithms/Medium/53_MaximumSubarray/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum, max_sub_sum = 0, nums[0]
+        for num in nums:
+            if current_sum < 0:
+                current_sum = 0
+            current_sum += num 
+            max_sub_sum = max(current_sum, max_sub_sum)
+
+        return max_sub_sum
