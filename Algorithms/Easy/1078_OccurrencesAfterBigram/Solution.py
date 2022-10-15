81cafb6ec2285feb21f3cde6cd07a52cdc9f4e88
Joy Krishan Das <joykrishan10@gmail.com>
2022-10-15 13:37:45 +0600

    Changes added to Python solution for 1078. OccurrencesAfterBigram

--- Algorithms/Easy/1078_OccurrencesAfterBigram/Solution.py
+++ Algorithms/Easy/1078_OccurrencesAfterBigram/Solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
-        arrayOfWords = text.split()
-        thirdWords = list()
-        for i in range(1, len(arrayOfWords) - 1):
-            if arrayOfWords[i] == second and arrayOfWords[i-1] == first:
-                thirdWords.append(arrayOfWords[i+1])
-        return thirdWords
-                
+        array_of_words = text.split()
+        third_words = list()
+        for i in range(1, len(array_of_words) - 1):
+            if array_of_words[i] == second and array_of_words[i-1] == first:
+                third_words.append(array_of_words[i+1])
+
+        return third_words         
