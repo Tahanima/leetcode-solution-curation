969ad0f3244734e7ea215e556d9ee52e73235e8c
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2023-06-28 00:48:40 +0600

    Update Solution.java

--- Algorithms/Medium/11_Container With Most Water/Solution.java
+++ Algorithms/Medium/11_Container With Most Water/Solution.java
@@ -2,24 +2,20 @@ class Solution {
     public int maxArea(int[] height) {
      int i = 0;
      int j = height.length - 1;
-
      int maxArea = Integer.MIN_VALUE; 
 
-     while(i < j) { 
+     while (i < j) { 
          int h = Math.min(height[i], height[j]);
          int width = j - i;
-
          maxArea = Math.max(maxArea, h*width);
 
-         if(height[i] > height[j]) {
+         if (height[i] > height[j]) {
              j--;
          } else {
              i++;
          }
-
      }
 
      return maxArea;
-
     }
 }
