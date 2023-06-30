367b2c44d9982567d6a86f2de582f7c9c0b233af
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2023-07-01 01:20:33 +0600

    Update Solution.py

--- Algorithms/Easy/242_ValidAnagram/Solution.py
+++ Algorithms/Easy/242_ValidAnagram/Solution.py
@@ -6,8 +6,8 @@ class Solution:
         count_t = {}
         count_s = {}
         for i in range(len(t)):
-             count_t[t[i]] = count_t.get(t[i], 0) + 1
-             count_s[s[i]] = count_s.get(s[i], 0) + 1
+            count_t[t[i]] = count_t.get(t[i], 0) + 1
+            count_s[s[i]] = count_s.get(s[i], 0) + 1
         
         for key in count_s.keys():
             if count_t.get(key) == None or count_t[key] != count_s[key]:
