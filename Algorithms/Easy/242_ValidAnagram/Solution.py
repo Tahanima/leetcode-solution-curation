fa49b4a438135775481b848500cd91fe648cb7a4
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2023-07-01 01:20:51 +0600

    Update Solution.py

--- Algorithms/Easy/242_ValidAnagram/Solution.py
+++ Algorithms/Easy/242_ValidAnagram/Solution.py
@@ -5,6 +5,7 @@ class Solution:
 
         count_t = {}
         count_s = {}
+        
         for i in range(len(t)):
             count_t[t[i]] = count_t.get(t[i], 0) + 1
             count_s[s[i]] = count_s.get(s[i], 0) + 1
