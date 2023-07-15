c615d9b2538ffbdf8452d9975274ec26c152b477
Joy Krishan Das <joykrishan10@gmail.com>
2023-07-15 21:59:35 +0600

    Added Python solution to 36. Valid Sudoku

---
+++ Algorithms/Medium/36_ValidSudoku/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        sqrs = collections.defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                
+                if (board[i][j] in rows[i] or 
+                    board[i][j] in cols[j] or 
+                    board[i][j] in sqrs[(i // 3, j // 3)]):
+                    return False
+                
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                sqrs[(i // 3, j // 3)].add(board[i][j])
+
+        return True
